f773266befb380ab66e3d6afe91696dc934ce571
thedemonsid <siddheshshrirame@gmail.com>
2024-12-21 01:42:59 +0530

    feat: add Tailwind CSS and PostCSS configuration, remove unused styles, and update App component

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,26 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-  window.electron.test();
-  return (
-    <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div className="bg-red-600">Siddhesh</div>;
 }
 
 export default App;
