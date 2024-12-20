817cb131594c7393d324d997bf8bae75c9d8a285
thedemonsid <siddheshshrirame@gmail.com>
2024-12-20 23:09:33 +0530

    add Electron configuration and enhance main process security settings

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,6 +2,12 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      sandbox: true,
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
   mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
 });
