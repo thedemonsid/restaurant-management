7d2759484ae48cba25ef18ab3a3b20f3dd5a835b
thedemonsid <siddheshshrirame@gmail.com>
2024-12-21 01:50:28 +0530

    feat: add shadcn ui

--- src/ui/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="bg-red-600">Siddhesh</div>;
-}
-
-export default App;
---
+++ src/web/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from "../components/ui/button";
+
+function App() {
+  return <Button className="bg-red-600">Siddhesh</Button>;
+}
+
+export default App;
